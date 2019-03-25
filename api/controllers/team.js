82e203bad82a61b056c25b6a15f59e54370172e6
HasibKUCSE <hasibsdvrt555@gmail.com>
2019-03-26 01:48:49 +0600

    Input validation added using joi

--- api/controllers/team.js
+++ api/controllers/team.js
@@ -1,4 +1,16 @@
+const Joi = require('joi');
+
 const models = require('../../models');
+
+function validation_schema(){
+    const schema = Joi.object().keys({
+    name: Joi.string()
+    .regex(/^[a-zA-Z ]*$/)
+    .required()
+    });
+
+    return schema;
+}
 
 exports.get_all_Teams = async (ctx, next) => {
     const teams = await models.Team.findAll()
@@ -22,6 +34,19 @@ exports.get_team_by_id = async (ctx, next) => {
 };
 
 exports.update_team_by_id = async (ctx, next) => {
+
+    schema = validation_schema();
+
+    const {value, error} = Joi.validate(ctx.request.body.team, schema);
+
+    if (error && error.details) {
+      ctx.status=404;
+      ctx.message="Invalid Input or bad request";
+      ctx.body={
+        error
+      }
+    }
+    else{
     const id = ctx.params.id
     let team = await models.Team.findOne({where: { id }})
     team = await team.update(ctx.request.body.team)
@@ -31,16 +56,30 @@ exports.update_team_by_id = async (ctx, next) => {
     ctx.status=200;
     ctx.message="Successfully Updated, id: " + id;
     await next();
+  }
 };
 
 exports.create_team = async (ctx, next) => {
-    const team = await models.Team.create(ctx.request.body.team)
-    ctx.body = {
-      team
+    schema = validation_schema();
+
+    const {value, error} = Joi.validate(ctx.request.body.team, schema);
+
+    if (error && error.details) {
+      ctx.status=404;
+      ctx.message="Invalid Input or bad request";
+      ctx.body={
+        error
+      }
     }
-    ctx.status=200;
-    ctx.message="Successfully Created";
-    await next();
+    else{
+      const team = await models.Team.create(ctx.request.body.team)
+      ctx.body = {
+        team
+      }
+      ctx.status=200;
+      ctx.message="Successfully Created";
+      await next();
+    }
 };
 exports.delete_team_by_id = async (ctx, next) => {
     const id = ctx.params.id
